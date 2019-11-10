009bc175766a6260099d8e2927d66669797a6141
mjgreene2010 <mjgreene2010@gmail.com>
2019-11-09 20:38:33 -0600

    Finished

--- Lucky_Sevens/luckySevens.js
+++ Lucky_Sevens/luckySevens.js
@@ -7,15 +7,62 @@ clearErrors = () => {
   }
 };
 
-rollDice = () => {
-  Math.floor(Math.random() * 6) + Math.floor(Math.random() * 6);
+rolling = gameMoney => {
+  totalRolls = 0;
+
+  highest = gameMoney;
+  highCount = 0;
+
+  diceTotal = () => {
+    return (
+      Math.floor(Math.random() * 6 + 1) + Math.floor(Math.random() * 6 + 1)
+    );
+  };
+
+  while (gameMoney > 0) {
+    totalRolls++;
+    if (diceTotal() == 7) {
+      gameMoney += 4;
+      if (gameMoney > highest) {
+        highest = gameMoney;
+        highCount = totalRolls;
+      }
+    } else {
+      gameMoney -= 1;
+    }
+  }
+
+  document.getElementById('total-rolls-number').innerText = totalRolls;
+
+  document.getElementById('highest-amount-won-number').innerText =
+    '$' + highest;
+
+  document.getElementById(
+    'roll-count-at-highest-amount-won-number'
+  ).innerText = highCount;
+
+  // if (gameMoney === 0) {
+  //   alert('Game Over');
+  // }
 };
 
-function play() {
+validateBet = () => {
   clearErrors();
   var startingBet = document.forms['lucky-form']['initial-starting-bet'].value;
+  let gameMoney = Number(startingBet);
+
+  if (isNaN(startingBet) || startingBet <= 0) {
+    alert('Enter a number greater than 0');
+    document.forms['lucky-form'][
+      'initial-starting-bet'
+    ].parentElement.className = 'form-group has-error';
+    document.forms['lucky-form']['initial-starting-bet'].focus();
+    return false;
+  }
 
   document.getElementById('starting-bet-number').innerText =
     '$' + `${startingBet}`;
+
+  rolling(gameMoney);
   return false;
-}
+};
